f0b6f9b2f9a050b89b4616dffc97e1f00e1ab183
Phillip Wang <pwang09@outlook.com>
2026-07-18 22:03:28 -0700

    Added a working button that displays popup when clicked

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 export default function App() {
+  function handleClick() {
+    alert("You clicked the button!");
+  }
+
   return (
     <>
-      <h1 style={{color:"green", fontFamily:"helvetica", fontSize:"50px", textAlign:"center" }}>This is a heading.</h1>
-      <p style={{color:"purple"}}>web dev</p>
+      <h1 style={{ color: "green", fontFamily: "helvetica", fontSize: "50px", textAlign: "center" }}>This is a heading.</h1>
+      <p style={{ color: "purple" }}>web dev</p>
+      <div style={{ padding: '1em' }}>
+        <button style={{ fontFamily: "fantasy" }} onClick={handleClick}>
+          Click Me
+        </button>
+      </div>
     </>
   );
 }
