fdd36e5b5b334b180d3b1b47182c3cba0bef70c4
Phillip Wang <pwang09@outlook.com>
2026-07-18 20:54:33 -0700

    Adjusted text using inline CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <>
-      <h1 style={{color:"green"}}>This is a heading.</h1>
+      <h1 style={{color:"green", fontFamily:"helvetica", fontSize:"50px", textAlign:"center" }}>This is a heading.</h1>
       <p style={{color:"purple"}}>web dev</p>
     </>
   );
