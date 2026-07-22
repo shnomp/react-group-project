bfa9aa5b3a36d19b64f750df782d3218c17ecdac
Phillip Wang <pwang09@outlook.com>
2026-07-21 21:28:26 -0700

    Added more traits and formatted buttons in a grid

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,13 @@ export default function App() {
   return (
     <div className='comic-sans-font'>
       <h1>the meme calculator</h1>
-      <p>use at your own risk</p>
-      <div>
+      <h2 style={{fontSize: "16px"}}>click two different traits to combine them to output a meme</h2>
+      <p>(use at your own risk)</p>
+      <div className='traits-grid'>
         <button>absurd</button>
         <button>confusing</button>
+        <button>classic</button>
+        <button>modern</button>
       </div>
     </div>
   );
