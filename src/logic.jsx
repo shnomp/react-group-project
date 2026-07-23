9e5468d41ab7fb1951c1c0429d7e4ae0aa58acfb
Phillip Wang <pwang09@outlook.com>
2026-07-22 18:55:13 -0700

    Added images and gifs for memes into assets folder

--- src/logic.jsx
+++ src/logic.jsx
@@ -1,6 +1,6 @@
 export function calculate(trait1,trait2){
     let output = "";
-    // 
+    // "Be Careful Around the Fabric of Reality, Garfield"
     if ((trait1 === "absurd" && trait2 === "strange") || (trait1 === "strange" && trait2 === "absurd")) {
         output = "1";
     }
@@ -8,18 +8,19 @@ export function calculate(trait1,trait2){
     else if ((trait1 === "classic" && trait2 === "modern") || (trait1 === "modern" && trait2 === "classic")) {
         output = "2";
     }
-    // 67 meme
+    // 67 Meme
     else if ((trait1 === "absurd" && trait2 === "modern") || (trait1 === "modern" && trait2 === "absurd")) {
         output = "3";
     }
-    // Italian brainrot
+    // Italian Brainrot
     else if ((trait1 === "modern" && trait2 === "strange") || (trait1 === "strange" && trait2 === "modern")) {
         output = "4";
     }
-    // 
+    // And His Name is John Cena
     else if((trait1 === "absurd" && trait2 === "classic") || (trait1 === "classic" && trait2 === "absurd")) {
         output = "5";
     }
+    // Big Chungus
     else if((trait1 === "strange" && trait2 === "classic") || (trait1 === "classic" && trait2 === "strange")) {
         output = "6";
     }
